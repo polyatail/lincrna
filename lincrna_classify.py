a56fea68ac9b85a678a5c8ff70be2532b44742ee
polyatail <andrew.sczesnak@med.nyu.edu>
2011-09-19 17:08:53 -0400

    Initial commit

---
+++ lincrna_classify.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Andrew Sczesnak"
+__email__ = "andrew.sczesnak@med.nyu.edu"
+__date__ = "9/19/2011"
+__version__ = 0.0
+
+from optparse import OptionParser
+
+parser = OptionParser(usage="%prog [options] <working_directory> <transcripts.gtf>",
+                      version="%prog " + str(__version__))
+
+# (1) Size selection
+parser.add_option("--min-exons",
+                  dest="min_exons",
+                  type="int",
+                  metavar="[2]",
+                  default=2,
+                  help="minimum number of exons")
+                  
+parser.add_option("--min-size",
+                  dest="min_size",
+                  type="int",
+                  metavar="[200]",
+                  default=200,
+                  help="minimum transcript length")
+                  
+# (2) Minimal read coverage threshold
+parser.add_option("--min-cov",
+                  dest="min_cov",
+                  type="int",
+                  metavar="[3]",
+                  default=3,
+                  help="minimum transcript coverage")
+                  
+# (3) Filter of known non-lincRNA annotations
+parser.add_option("--filter",
+                  dest="filter",
+                  action="append",
+                  metavar="filter.gtf",
+                  help="remove transcripts with exons overlapping these GTFs")
+                  
+# (4) Positive coding potential threshold
+parser.add_option("--min-csf",
+                  dest="min_csf",
+                  type="float",
+                  metavar="[10.0]",
+                  help="minimum CSF score of transcript")
+                  
+# (5) Known protein domain filter
+parser.add_option("--exclude-pfam",
+                  dest="exclude_pfam",
+                  action="store_true",
+                  default=False,
+                  help="remove transcripts containing Pfam domains")
+                  
+options, args = parser.parse_args()
+
+print options
